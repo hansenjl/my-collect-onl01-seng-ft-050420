b63475a442ab9b611c87a6106c12754d6ae8c0fc
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-07 15:50:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
   idx = 0  
   new_array = []
   while idx < array.length 
-    new_array << yield(array[idx], id + 5) 
+    new_array << yield(array[idx]) 
     idx += 1 
   end
   new_array
